0b9d30a308537173938b8230ef1d0cac3fa92735
Emanuel Turtula <emanuelturtula@gmail.com>
2019-11-28 19:15:51 -0300

    Agregado funciones de UART

---
+++ inc/uart.h
@@ -0,0 +1,10 @@
+#ifndef _UART_H_
+#define _UART_H_
+
+#include "PrefixRequired.h"
+
+#include "board_connections.h"
+
+#include "utils.h"
+
+#endif
---
+++ src/uart.c
@@ -0,0 +1,19 @@
+#include "uart.h"
+
+extern void prefixInternal_opSend_Status(const Prefix* handle, const sc_integer st, const sc_integer temp, const sc_integer setpoint) {
+	char T[10];			//Variable donde se guarda el dato convertido a string
+	char SP[10];
+	char * STATUS;
+	itoa(temp, T,10);		//Convierto a una string
+	itoa(setpoint, SP,10);		//Convierto a una string
+	STATUS = "OFF";
+	if (st == 1) {
+		STATUS = "ON";
+	}
+	uartWriteString( UART_USB, STATUS );
+	uartWriteString( UART_USB, "," );
+	uartWriteString( UART_USB, T );
+	uartWriteString( UART_USB, "," );
+	uartWriteString( UART_USB, SP );
+	uartWriteString( UART_USB, "\r\n" );
+}
